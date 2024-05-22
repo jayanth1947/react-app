a3d080d56727db372c877960d00e55ce525d449d
jayanth1947 <jayanthonteru111@gmail.com>
2024-05-22 14:57:31 +0530

    sample-component-class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { ReactDOM } from "react";
 import { render } from 'react-dom';
 
@@ -34,9 +34,47 @@ const newJSXElement =
   </div>
 
 
+//Create a class for the List
+class List extends Component {
+
+  render() {
+
+    return (
+      <ol>
+        {elemList.map((ele, index) => <li key={index} > {ele} </li>)}
+      </ol>)
+  }
+}
+
+
+//Create a class for the Title
+class Title extends Component {
+
+  render() {
+    return (
+      <h1>Task List</h1>
+    )
+  }
+}
+
+
+//Create a class for the Main DOM to run by embedding all the components
+class Main extends Component {
+  render() {
+
+    return (
+      <div>
+        <Title />
+        <List />
+      </div>
+    )
+  }
+}
+
+
 
 // Render the above elements in the browser
 //render(element, document.getElementById('root'));
 //render(paraElement, document.getElementById('root'));
-render(newJSXElement, document.getElementById('root'));
+render(<Main />, document.getElementById('root'));
 

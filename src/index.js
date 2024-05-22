733a72d08f84ba5f7b3caf66031f948dfd1b9705
jayanth1947 <jayanthonteru111@gmail.com>
2024-05-22 12:08:23 +0530

    array-elements

--- src/index.js
+++ src/index.js
@@ -8,16 +8,21 @@ const element = React.createElement('h1', null, 'Hello world');
 // For Paragraph element
 const paraElement = React.createElement('p', null, 'The world is so beautiful');
 
+const elemList = ['Mango', 'Apple', 'Banana'];
+
+
 
 // For Order List elements
 const listElement = React.createElement('ol', null,
-  React.createElement('li',null,'Mango'),
-  React.createElement('li',null,'Apple'),
-  React.createElement('li',null,'Banana'),);
+  elemList.map((ele) => React.createElement('li', null, ele)));
+
+// List using index
+const newlistElement = React.createElement('ol', null,
+  elemList.map((ele,index) => React.createElement('li', { key: index }, ele)));
 
 
 // Render the above elements in the browser
 render(element, document.getElementById('root'));
 render(paraElement, document.getElementById('root'));
-render(listElement, document.getElementById('root'));
+render(newlistElement, document.getElementById('root'));
 

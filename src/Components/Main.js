82b8bca496a3ff2bcae9ee5369caba1fdd153622
jayanth1947 <jayanthonteru111@gmail.com>
2024-05-22 18:56:14 +0530

    component-architecture

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
-import List from "./List";
+
 import Title from "./Title";
+import Photowall from "./photowall";
 
-    //Create a class for the Main DOM to run by embedding all the components
-    class Main extends Component {
-        render() {
+const posts = [{
+    id: "0",
+    description: "beautiful landscape",
+    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+        "3919321_1443393332_n.jpg"
+}, {
+    id: "1",
+    description: "Aliens???",
+    imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
+}, {
+    id: "2",
+    description: "On a vacation!",
+    imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+}]
 
-            return (
-                <div>
-                    <Title title={'Games'} />
-                    <List elemList={['Cricket', 'Batminton', 'Chess']} />
-                    <List elemList={['Tennis', 'Hockey', 'Carroms']} />
-                </div>
-            )
-        }
+
+
+//Create a class for the Main DOM to run by embedding all the components
+class Main extends Component {
+    render() {
+
+        return (
+            <div>
+                <Title title={'Photowall'} />
+                <Photowall posts={posts}/>
+            </div>
+        )
     }
+}
 
 export default Main
--- src/index.js
+++ src/index.js
@@ -2,37 +2,38 @@ import React, { Component } from "react";
 import { ReactDOM } from "react";
 import { render } from 'react-dom';
 import Main from "./Components/Main";
+import './styles/stylesheet.css';
 
-//For Header element
-const element = React.createElement('h1', null, 'Hello world');
+// //For Header element
+// const element = React.createElement('h1', null, 'Hello world');
 
-// For Paragraph element
-const paraElement = React.createElement('p', null, 'The world is so beautiful');
+// // For Paragraph element
+// const paraElement = React.createElement('p', null, 'The world is so beautiful');
 
-const elemList = ['Mango', 'Apple', 'Banana'];
-
-
-
-// For Order List elements
-const listElement = React.createElement('ol', null,
-  elemList.map((ele) => React.createElement('li', null, ele)));
-
-// List using index
-const newlistElement = React.createElement('ol', null,
-  elemList.map((ele, index) => React.createElement('li', { key: index }, ele)));
+// const elemList = ['Mango', 'Apple', 'Banana'];
 
 
 
-// JSX => It looks like a syntax of the HTML
-//Here we are embedding the array inside the JSX and this prints the array as we expected
+// // For Order List elements
+// const listElement = React.createElement('ol', null,
+//   elemList.map((ele) => React.createElement('li', null, ele)));
 
-const newJSXElement =
-  <div>
-    <h1>New List</h1>
-    <ol>
-      {elemList.map((ele, index) => <li key={index} > {ele}</li>)}
-    </ol>
-  </div>
+// // List using index
+// const newlistElement = React.createElement('ol', null,
+//   elemList.map((ele, index) => React.createElement('li', { key: index }, ele)));
+
+
+
+// // JSX => It looks like a syntax of the HTML
+// //Here we are embedding the array inside the JSX and this prints the array as we expected
+
+// const newJSXElement =
+//   <div>
+//     <h1>New List</h1>
+//     <ol>
+//       {elemList.map((ele, index) => <li key={index} > {ele}</li>)}
+//     </ol>
+//   </div>
 
 
 // Render the above elements in the browser
